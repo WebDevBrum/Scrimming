d3552ddecc4c3796c6025321360e015872c7323f
Scott Davies <scottdavies1983@gmail.com>
2021-02-26 18:28:52 +0000

    changed to class component and added conditional render

--- Module11-React/Learn-React/React-State-Practice-2/App.js
+++ Module11-React/Learn-React/React-State-Practice-2/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 // Given a stateless functional component, add state to it
 // state should have a property called `isLoggedIn` which is a boolean
@@ -6,12 +6,21 @@ import React from "react"
 // Then, give your best shot at rendering the word "in" if the user is logged in
 // or "out" if the user is logged out.
 
-function App() {
-    return (
-        <div>
-            <h1>You are currently logged (in/out)</h1>
-        </div>
-    )
+class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+      
+    render() {
+        return (
+            <div>
+                <h1>You are currently logged {this.state.isLoggedIn ? "in" : "out"}.</h1>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
