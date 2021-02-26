34fa445bc2a47d945bb2800eba6f442eb67532c1
Scott Davies <scottdavies1983@gmail.com>
2021-02-26 18:59:59 +0000

    setup of phase 5 tutorial

--- Module11-React/Learn-React/React-todo-app/TodoItem.js
+++ Module11-React/Learn-React/React-todo-app/TodoItem.js
@@ -1,9 +1,13 @@
+/**
+ * Challenge: Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.
+ */
+
 import React from "react"
 
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" defaultChecked={props.item.completed}/>
+            <input type="checkbox" checked={props.item.completed}/>
             <p>{props.item.text}</p>
         </div>
     )
