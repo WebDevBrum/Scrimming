368a058ee5f8b05d65e48d830d398dd38a98c60e
Scott Davies <scottdavies1983@gmail.com>
2021-02-28 19:48:11 +0000

    previous commit resulted in prevState being directly modified, issue resolved

--- Module11-React/Learn-React/React-todo-app/App.js
+++ Module11-React/Learn-React/React-todo-app/App.js
@@ -24,17 +24,18 @@ class App extends React.Component {
     }
     
     handleChange(id) {
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-        console.log("change", id)
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.map((todo) => {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
                 if (todo.id === id) {
-                    todo.completed = !todo.completed
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
                 }
                 return todo
             })
-            console.log(updatedTodos);
+            console.log(prevState.todos)
+            console.log(updatedTodos)
             return {
                 todos: updatedTodos
             }
@@ -42,8 +43,7 @@ class App extends React.Component {
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
-        handleChange={this.handleChange}/>)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
         
         return (
             <div className="todo-list">
