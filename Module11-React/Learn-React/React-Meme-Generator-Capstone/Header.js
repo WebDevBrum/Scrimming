7ad88b986fb6a489a745b154f7acf04dbc78cc57
Scott Davies <scottdavies1983@gmail.com>
2021-03-10 19:56:10 +0000

    completed meme generator project

--- Module11-React/Learn-React/React-Meme-Generator-Capstone/Header.js
+++ Module11-React/Learn-React/React-Meme-Generator-Capstone/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header>
             <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQEzYX0v41CQ1Q/profile-displayphoto-shrink_800_800/0/1516646865689?e=1620864000&v=beta&t=F1pdFllVANuW6O6QbG1I7mTISO7TB75nS80xUneF1yI" 
                 alt="Problem?"
             />
             <p>Meme Generator</p>
--- Module11-React/Learn-React/React-Meme-Generator-Capstone/MemeGenerator.js
+++ Module11-React/Learn-React/React-Meme-Generator-Capstone/MemeGenerator.js
@@ -9,6 +9,8 @@ class MemeGenerator extends Component {
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     componentDidMount() {
@@ -16,32 +18,55 @@ class MemeGenerator extends Component {
             .then(response => response.json())
             .then(response => {
                 const {memes} = response.data
-                console.log(memes[0])
                 this.setState({ allMemeImgs: memes })
+                // console.log(memes[1].url)
             })
+    }
+    
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({ [name]: value })
+    }
+    
+    /**
+     * Create a method that, when the "Gen" button is clicked, chooses one of the
+     * memes from our `allMemeImgs` array at random and makes it so that is the
+     * meme image that shows up in the bottom portion of our meme generator site (`.url`)
+     */
+    
+    handleSubmit(event) {
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length); 
+        const memeImage = this.state.allMemeImgs[randNum].url
+        event.preventDefault();
+        this.setState({randomImg: memeImage})
     }
     
     render() {
         return (
-            <div>
+            <div onSubmit={this.handleSubmit}>
                 <form className="meme-form">
-                    {
-                        /**
-                         * Create 2 input fields, one for the topText and one for the bottomText
-                         * Remember that these will be "controlled forms", so make sure to add
-                         * all the attributes you'll need for that to work
-                         */
-                    }  
-                    <label> 
-                    Top text: 
-                        <input type="text" name="topText" value={this.state.topText} onChange={console.log("text")}/>
-                    </label>
-                    <label>
-                    Bottom text:
-                        <input type="text" name="bottomText" value={this.state.bottomText} onChange={console.log("text")} />
-                    </label>
+                    <input 
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                    /> 
+                    <input 
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                    /> 
+                
                     <button>Gen</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         )
     }
