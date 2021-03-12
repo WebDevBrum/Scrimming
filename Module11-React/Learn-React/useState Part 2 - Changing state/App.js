3e47ecbb895129fa897aa7e842effdc83ee72d8a
Scott Davies <scottdavies1983@gmail.com>
2021-03-12 18:42:24 +0000

    example of further state and further functions

--- Module11-React/Learn-React/useState Part 2 - Changing state/App.js
+++ Module11-React/Learn-React/useState Part 2 - Changing state/App.js
@@ -5,6 +5,7 @@ import React, {useState} from "react"
 
 function App() {
     const [count, setCount] = useState(0)
+    const [answer, setAnswer] = useState("Yes")
     
     function increment() {
         setCount(prevCount => prevCount + 1)
@@ -14,11 +15,21 @@ function App() {
         setCount(prevCount => prevCount - 1)
     }
     
+    function double() {
+        setCount(prevCount => prevCount * 2)
+    }
+    
+    function half() {
+        setCount(prevCount => prevCount /2)
+    }
+    
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
+            <button onClick={double}>Double</button>
+            <button onClick={half}>Half</button>
         </div>
     )
 }
@@ -27,7 +38,8 @@ function App() {
 //     constructor() {
 //         super()
 //         this.state = {
-//             count: 0
+//             count: 0,
+//             answer: "Yes"
 //         }
 //     }
     
