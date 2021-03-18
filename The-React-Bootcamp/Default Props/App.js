f9b1bb97a970b2e61dc0e55abd319030b06e563c
Scott Davies <scottdavies1983@gmail.com>
2021-03-18 18:47:05 +0000

    default props setup

---
+++ The-React-Bootcamp/Default Props/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "./Card"
+
+function App() {
+    return (
+        <div>
+            <Card cardColor="red" height={} width={} />
+            <Card />
+            <Card cardColor="green" />
+        </div>
+    )
+}
+
+export default App
---
+++ The-React-Bootcamp/Default Props/Card.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Card(props) {
+    const styles = {
+        backgroundColor: props.cardColor,
+        height: 100,
+        width: 100
+    }
+    
+    return (
+        <div style={styles}></div>
+    )
+}
+
+Card.defaultProps = {
+    cardColor: "blue"
+}
+
+export default Card
