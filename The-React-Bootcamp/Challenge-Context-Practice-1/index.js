ce3509354bbcd017d7a12603a6528a9537581784
Scott Davies <scottdavies1983@gmail.com>
2021-03-30 06:46:38 +0100

    setup of contetx activity

---
+++ The-React-Bootcamp/Challenge-Context-Practice-1/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+/**
+ * Challenge: Set up context to save the user's username and pass it to anywhere that is currently hardcoding "Username".
+ * 
+ * Use the static class property `contextType` on any components that need to consume context.
+ * https://reactjs.org/docs/context.html
+ */
+
+ReactDOM.render(<App />, document.getElementById("root"))
