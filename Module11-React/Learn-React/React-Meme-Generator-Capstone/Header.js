cd62ab191ce4e9dcf424d8af963d8564ed9e4d7e
Scott Davies <scottdavies1983@gmail.com>
2021-04-25 23:54:25 +0100

    html and css challenges warm up

--- Module11-React/Learn-React/React-Meme-Generator-Capstone/Header.js
+++ Module11-React/Learn-React/React-Meme-Generator-Capstone/Header.js
@@ -3,10 +3,6 @@ import React from "react"
 function Header() {
     return (
         <header>
-            <img 
-                src="https://media-exp1.licdn.com/dms/image/C4E03AQEzYX0v41CQ1Q/profile-displayphoto-shrink_800_800/0/1516646865689?e=1620864000&v=beta&t=F1pdFllVANuW6O6QbG1I7mTISO7TB75nS80xUneF1yI" 
-                alt="Problem?"
-            />
             <p>Meme Generator</p>
         </header>
     )
